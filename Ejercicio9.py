ae5f76b7f7eab3da7f7dc074c213dcbb66ae83a7
vegeta008 <55161730+vegeta008@users.noreply.github.com>
2021-02-16 13:51:04 -0500

    Update Ejercicio9.py

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -1,3 +1,5 @@
+#   9.Leer la temperatura en grados Celsius y convertirla a grados Kelvin y a grados Farenheit
+
 grados = int(input('Ingrese la temperatura en grados Celsius: '))
 Kelvin = 273 + grados
 fahrenheit = 9.0/5.0 * grados +32
