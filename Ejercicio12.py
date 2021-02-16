e59968e53b72698da2f48a147ea484c1b7ce8f9e
vegeta008 <55161730+vegeta008@users.noreply.github.com>
2021-02-16 13:52:48 -0500

    Update Ejercicio12.py

--- Ejercicio12.py
+++ Ejercicio12.py
@@ -1,3 +1,7 @@
+#   12.Calcular el sueldo total a recibir de un empleado.  El usuario deberá ingresar el número de horas trabajadas y 
+#   el valor por cada hora. Considere en los cálculos el descuento de seguridad social y una bonificación del 2% para 
+#   aquellos cuyo sueldo no supere los 300 dólares.
+
 
 resultado = "si"
 while resultado != "no":
